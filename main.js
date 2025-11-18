05528a6e5a72e4c49e955516bacce1c880c2ce79
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-11-18 17:31:58 +0100

    render one task

--- main.js
+++ main.js
@@ -5,6 +5,31 @@
 
 const usersList = document.querySelector(".js_usersList");
 
+// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
+// Este código se ejecutará cuando se carga la página
+// Lo más común es:
+//   - Pedir datos al servidor
+//   - Pintar (render) elementos en la página
+
+const oneuser = {
+  name: "Yolande Dupuis",
+  city: "Mettembert",
+  username: "greencat145",
+};
+
+usersList.innerHTML = `
+
+<li class="userCard">
+        <img src="./images/Druv Chiplunkar.jpg" alt="Userphoto" />
+        <h3>${oneuser.name}</h3>
+        <ul class="usersfeatures">
+          <li>${oneuser.city}</li>
+          <li>${oneuser.username}</li>
+        </ul>
+      </li>`;
+
+usersListUL.innerHTML = console.log("Página y JS cargados!");
+
 // SECCIÓN DE DATOS
 // Variables globales que almacenan la información principal de la aplicación
 // y se usan por todo el fichero.
@@ -21,11 +46,3 @@ const usersList = document.querySelector(".js_usersList");
 // SECCIÓN DE EVENTOS
 // Éstos son los eventos a los que reacciona la página
 // Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
-
-// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
-// Este código se ejecutará cuando se carga la página
-// Lo más común es:
-//   - Pedir datos al servidor
-//   - Pintar (render) elementos en la página
-
-console.log("Página y JS cargados!");
